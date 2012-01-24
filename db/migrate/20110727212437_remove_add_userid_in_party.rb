feec65e4aff43848560fc5e98fad484f6562d418
edwardpabst <ed.pabst@yahoo.com>
2012-01-24 15:36:36 -0800

    mig 3

--- db/migrate/20110727212437_remove_add_userid_in_party.rb
+++ db/migrate/20110727212437_remove_add_userid_in_party.rb
@@ -1,11 +1,11 @@
 class RemoveAddUseridInParty < ActiveRecord::Migration
   def self.up
-    remove_column :parties, :userid
-    add_column :parties, :user_id, :integer
+    #remove_column :parties, :userid
+    #add_column :parties, :user_id, :integer
     
   end
 
   def self.down
-    remove_column :parties, :userid
+    #remove_column :parties, :userid
   end
 end
