cee45d704c41416d19576e80815681a12038bd5c
edwardpabst <ed.pabst@yahoo.com>
2012-01-24 16:08:01 -0800

    mig 4

--- db/migrate/20110728212437_add_countryid_in_party.rb
+++ db/migrate/20110728212437_add_countryid_in_party.rb
@@ -1,10 +1,17 @@
 class AddCountryidInParty < ActiveRecord::Migration
-  def self.up
-    add_column  :countries, :country_id, :integer 
-    remove_column :countries, :partycountry
-  
+    create_table :countries do |t|
+      t.string :code2
+      t.string :name
+      t.string :nameLC
+      t.string :code3
+      t.string :number
+      t.string :country_id
+
+      t.timestamps
+    end
   end
 
   def self.down
+    drop_table :countries
   end
 end
