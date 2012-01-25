6d71f9d2b4483588b23d3a3487039e9fe1646aca
edwardpabst <ed.pabst@yahoo.com>
2012-01-24 16:57:03 -0800

    mig 14

--- db/migrate/20120123075627_change_isfullservice_equipmentactivity.rb
+++ db/migrate/20120123075627_change_isfullservice_equipmentactivity.rb
@@ -1,6 +1,6 @@
 class ChangeIsfullserviceEquipmentactivity < ActiveRecord::Migration
   def self.up
-    remove_column :equipmentactivities, :activity_type
+    remove_column :equipmentactivities, :is_fullservice
     add_column :equipmentactivities, :activity_type, :string
   end
 
