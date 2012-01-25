db2d4f03ad840148a3fd90766b7463a432c75b2c
edwardpabst <ed.pabst@yahoo.com>
2012-01-24 16:42:11 -0800

    mig 12

--- db/migrate/20111029074713_add_weatherpostal_parties.rb
+++ db/migrate/20111029074713_add_weatherpostal_parties.rb
@@ -1,13 +1,13 @@
 class AddWeatherpostalParties < ActiveRecord::Migration
   def self.up
      
-    add_column :parties, :partyweatherpostalcode, :string
+     
   
    
   end
 
   def self.down
-    remove_column :parties, :partyweatherpostalcode
+    
  
     
   end
--- db/migrate/20111030000231_correct_weatherpostalcode_parties.rb
+++ db/migrate/20111030000231_correct_weatherpostalcode_parties.rb
@@ -1,14 +1,10 @@
 class CorrectWeatherpostalcodeParties < ActiveRecord::Migration
   def self.up
-    remove_column :parties, :partyweatherpostalcode
-    add_column :parties, :partyweatherpostalcode, :string
-  
+ 
    
   end
 
   def self.down
-    remove_column :parties, :partyweatherpostalcode
  
-    
   end
 end
