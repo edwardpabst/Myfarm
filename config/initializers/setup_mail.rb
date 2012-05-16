81eacc23f3c3cc51d2454ea5f692e47ae08b27e5
edwardpabst <ed.pabst@yahoo.com>
2012-05-16 15:35:38 -0700

    bsb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtpout.secureserver.net",
   :port                 => 465,
-  :domain               => "ifarmservice.com",
+  :domain               => "www.ifarmservice.com",
   :user_name            => "customer.support",
   :password             => "bubble2",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "www.ifarmservice.com"
 
