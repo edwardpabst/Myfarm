86d6fe89f1e1f66a7f6283ff923882fde9af13e9
edwardpabst <ed.pabst@yahoo.com>
2012-05-16 14:18:11 -0700

    pubkey

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,8 @@
 
-Stripe.api_key = "koIjBxx5VsUpfgLnOH4WlT2CeTu6wTdA"
-STRIPE_PUBLIC_KEY = "pk_7WOFyvthkLOFFoamkYzwYC8oDs0GR"
+#  Test keys
+# Stripe.api_key = "koIjBxx5VsUpfgLnOH4WlT2CeTu6wTdA"
+# STRIPE_PUBLIC_KEY = "pk_7WOFyvthkLOFFoamkYzwYC8oDs0GR"
 
+# Live production keys
+Stripe.api_key = "NiFblY5wPpIuoEi3iHwFJp1jij37tS4P"
+STRIPE_PUBLIC_KEY = "pk_7CUN1RpVqQWZJHrG4T35Wyci4WU0P"
