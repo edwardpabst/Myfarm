cd150a2c8e7963ac26effc39e1d7cb61fc19a63c
edwardpabst <ed.pabst@yahoo.com>
2012-05-16 16:40:38 -0700

    aaa

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,21 @@
+# ActionMailer::Base.smtp_settings = {
+#  :address              => "smtpout.secureserver.net",
+#  :port                 => 25,
+#  :domain               => "www.ifarmservice.com",
+#  :user_name            => "customer.support",
+#  :password             => "bubble2",
+ # :authentication       => "plain",
+#  :enable_starttls_auto => true
+# }
+
+# ActionMailer::Base.default_url_options[:host] = "www.ifarmservice.com"
+
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtpout.secureserver.net",
-  :port                 => 25,
-  :domain               => "www.ifarmservice.com",
-  :user_name            => "customer.support",
-  :password             => "bubble2",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => "app2615320@heroku.com",
+  :password       => "l1oeawmv",
+  :domain         => 'heroku.com'
 }
-
-ActionMailer::Base.default_url_options[:host] = "www.ifarmservice.com"
-
+ActionMailer::Base.delivery_method = :smtp
